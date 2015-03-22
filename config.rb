16299c1980a9e722e35f971af2b83776d299895a
AlbertDeng <albertdeng@kkbox.com>
2015-03-23 00:14:50 +0800

    0323

---
+++ config.rb
@@ -0,0 +1,44 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "javascripts"
+
+output_style = :expanded
+sourcemap = true
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+relative_assets = false
+sass_options = {:debug_info=>false}
+
+line_comments = false
+
+preferred_syntax = :sass
+fireapp_coffeescripts_dir = "coffeescripts"
+fireapp_coffeescript_options = {:bare=>false}
+fireapp_livescripts_dir = "livescripts"
+fireapp_livescript_options = {:bare=>false}
+fireapp_minifyjs_on_build = false
+fireapp_always_report_on_build = true
+sass_options = {:debug_info=>false}
+
+css_dir = "stylesheets" # by Fire.app 
+sass_dir = "sass" # by Fire.app 
+images_dir = "images" # by Fire.app 
+javascripts_dir = "javascripts" # by Fire.app 
+fireapp_coffeescripts_dir = "coffeescripts" # by Fire.app 
+fireapp_livescripts_dir = "livescripts" # by Fire.app 
+fireapp_minifyjs_on_build = false # by Fire.app 
+fireapp_always_report_on_build = true # by Fire.app 
+output_style = :expanded # by Fire.app 
+relative_assets = false # by Fire.app 
+line_comments = false # by Fire.app 
+sass_options = {:debug_info=>false} # by Fire.app 
+sourcemap = true # by Fire.app 
+fireapp_coffeescript_options = {:bare=>false} # by Fire.app 
+fireapp_livescript_options = {:bare=>true} # by Fire.app 
